b771629ee052358ea3a88d283118bc3845925b6e
Bev7787 <bevan.tsui@students.mq.edu.au>
2023-05-28 12:00:06 +1000

    Implement FF algorithm.

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
+import java.util.HashMap;
+
 public class Main {
 	public static void main(String[] args) {
 		String username = System.getProperty("user.name");
@@ -23,7 +25,11 @@ public class Main {
 				if (c.getResponseHeader(redyResponse).equals("JOBN")) {
 					String[] job = redyResponse.split(" ", 5);
 					// Get the server to assign to. Pass in core/memory/disk
-					c.getFirstServer(job[4]);
+					c.getFirstAvailServer(job[4]);
+					if (!(c.isAvailable)) {
+						System.out.println("job: " + redyResponse);
+						c.getFirstCapableServer(job[4]);
+					}
 		
 
 					c.scheduleJob(redyResponse);
@@ -43,12 +49,12 @@ public class Main {
 class Client {
 	private String username;
 	private String serverType = ""; // Largest server type
-	private int currID = 0;
-	private int highestID = 0;
+	private String currID = "0";
 	private DataOutputStream dout;
 	private BufferedReader in;
 	// Last received message
 	public String prevMessage = "";
+	public boolean isAvailable = true; 
 	
 	// Constructor takes in output stream, reader and username
 	public Client(DataOutputStream x, BufferedReader y, String uname) {
@@ -63,7 +69,7 @@ class Client {
 			dout.write((command + "\n").getBytes());
 			dout.flush();
 			
-			//System.out.println("OUT: " + command);
+			System.out.println("OUT: " + command);
 		}
 		catch (Exception e) {}
 	}
@@ -73,7 +79,7 @@ class Client {
 		try {
 			prevMessage = (String)in.readLine();
 			
-			//System.out.println("IN: " + prevMessage);
+			System.out.println("IN: " + prevMessage);
 		}
 		catch (Exception e) {}
 	}
@@ -100,7 +106,7 @@ class Client {
 		try {
 			String[] buffer = message.split(" ", 4);
 			String jobID = buffer[2];
-			sendOut("SCHD " + jobID + " " + serverType + " " + 0);
+			sendOut("SCHD " + jobID + " " + serverType + " " + currID);
 		}
 		catch (Exception e) {}
 	}
@@ -110,27 +116,56 @@ class Client {
 	// getFirstServer handles messaging the server to find the first capable server using GETS Capable
 	// IN: core/memory/disk of job
 	
-	// MODIFY TO BF by using GETS Capable? to identify servers, find server with smallest fitness value.
+	// MODIFY TO BF by using GETS Avail to identify servers, find server with smallest fitness value. (todo: Determine whether GETS Avail sorts by best fitness or in server order.
 	// Steps: On initial load, store maximum and current index of servers in 2D array. Load in job based on GETS Available. If none available,
-	// use GETS Capable to find first best capable server of a given type. Find server, increment id by 1 until max, in that case revert to 0. (VERY SLOW ON LARGE NUMBER OF SERVERS)
-	// use java dictionary with server type as key, value is tupule [current index, max size]
-	public void getFirstServer(String cmd) {
+	// use GETS Capable to find first best capable server of a given type. Find server, increment id by 1 until max, in that case revert to 0.
+	// use java HashMap with server type as key, value is tupule [current index, max size]
+	// todo: modify LRR find server algorithm to put total number of server and server type into key/value pairs. Use GETS available, and iff none, GETS capable with KVP.
+	public void getFirstCapableServer(String cmd) {
 		try {
+			isAvailable = true;
 			sendOut("GETS Capable" + " " + cmd);
 			// DATA nServer
 			receiveIn();
 			String[] buffer = prevMessage.split(" ", 3);
 			int serverNum = Integer.parseInt(buffer[1]);
 			sendOut("OK");
-			// read first serverType
-			receiveIn();
 			if (serverNum > 0) {
+				// read first serverType
+				receiveIn();
 				buffer = prevMessage.split(" ", 6); 
 				serverType = buffer[0];
+				currID = buffer[1];
 				for (int i = 1; i < serverNum; i++) 
 					receiveIn();
-			}		
+			}	
 			sendOut("OK");
+			receiveIn();
+		}
+		catch (Exception e) {}
+	}
+	
+	public void getFirstAvailServer(String cmd) {
+		try {
+			sendOut("GETS Avail" + " " + cmd);
+			// DATA nServer
+			receiveIn();	
+			String[] buffer = prevMessage.split(" ", 3);
+			int serverNum = Integer.parseInt(buffer[1]);
+			sendOut("OK");
+			
+			if (serverNum > 0) {
+				// read first serverType
+				receiveIn();
+				buffer = prevMessage.split(" ", 6); 
+				serverType = buffer[0];
+				currID = buffer[1];
+				for (int i = 1; i < serverNum; i++) 
+					receiveIn();
+				sendOut("OK");
+			}	
+			else
+				isAvailable = false;			
 			receiveIn();
 		}
 		catch (Exception e) {}
